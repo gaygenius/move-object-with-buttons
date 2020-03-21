6f6139a9ad1d4cc6a061e919f830eee5878a3ba5
Jose Capo <jose@josecapo.com>
2020-03-21 16:53:01 -0700

    Interview code from Codepen

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+console.clear();
+
+const InnerBox = ({ x, y }) => (
+  <div className="inner-box-container">
+    <div
+      style={{
+        height: '20px',
+        width: '20px',
+        backgroundColor: '#ff00bf',
+        position: 'relative',
+        left: x,
+        top: y,
+      }}
+    />
+  </div>
+);
+
+class OuterGrid extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      x: 100,
+      y: 100,
+    };
+  }
+
+  render() {
+    return (
+      <div className="outer-grid">
+        <div />
+        <div
+          className="grid-button"
+          onClick={e => {
+            if (this.state.y > 0) {
+              this.setState({ y: this.state.y - 10 });
+            }
+          }}
+        />
+        <div />
+        <div
+          className="grid-button"
+          onClick={e => {
+            if (this.state.x > 0) {
+              this.setState({ x: this.state.x - 10 });
+            }
+          }}
+        />
+        <InnerBox x={this.state.x} y={this.state.y} />
+        <div
+          className="grid-button"
+          onClick={e => {
+            if (this.state.x < 180) {
+              this.setState({ x: this.state.x + 10 });
+            }
+          }}
+        />
+      </div>
+    );
+  }
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <OuterGrid />;
 }
 
 export default App;
